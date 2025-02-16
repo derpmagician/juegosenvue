88dfb1058f8c0450602e7c084aa378bb85178ade
Derpmagician <renzin@gmail.com>
2025-02-16 08:24:40 -0500

    agregando base

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import GameView from '@/views/GameView.vue'
 
@@ -6,7 +6,7 @@ import GameView from '@/views/GameView.vue'
 
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(),
   routes: [
     {
       path: '/',
